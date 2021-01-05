eff09cdc161a8b06596bc483c81408b4c3832fb9
BrysonGreer95 <bryson.greer2014@gmail.com>
2021-01-05 08:57:06 -0500

    Solution for merge sorted array

---
+++ Arrays/MergeSortedArray/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int loopCounter = 0;
+        for (int i = m; i < nums1.length; i++) {
+            if (nums1[i] == 0) {
+                nums1[i] = nums2[loopCounter];
+            }
+            loopCounter++;
+        }
+        Arrays.sort(nums1);
+    }
+}
